29200ebcca886f00f892562bea6b0c0ed51bfeb5
théodore clément <213218350+thclemnt@users.noreply.github.com>
2026-07-07 14:16:28 +0200

    build: bump version

--- setup.php
+++ setup.php
@@ -2,7 +2,7 @@
 
 global $CFG_GLPI;
 
-define('DASHBOARDNG_VERSION', '1.1.11');
+define('DASHBOARDNG_VERSION', '1.1.12');
 define('DASHBOARDNG_ITSMNG_MIN_VERSION', '2.0');
 
 $hostLoader = require __DIR__ . '/../../vendor/autoload.php';
